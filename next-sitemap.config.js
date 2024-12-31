a5f250c0ba3287f257ceaf600df93e162e65ec41
Kailash1233 <kailash61203@gmail.com>
2024-12-31 12:05:10 +0530

    new domain seo fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://adszoo.vercel.app', // Replace with your site's URL
+    siteUrl: process.env.SITE_URL || 'https://adszoo.in', // Replace with your site's URL
     generateRobotsTxt: true, // Generate robots.txt file
     sitemapSize: 7000, // Limit the number of URLs per sitemap (if needed)
   };
